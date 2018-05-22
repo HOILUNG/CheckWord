a3037816219d4833b5d8d32e69334b3a1d40ceb0
HOILUNG <hailonghuang@live.com>
2018-05-22 15:43:48 +0800

    add

--- CheckWord/Form1.cs
+++ CheckWord/Form1.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using ServiceStack;
-using System.Text.RegularExpressions;
-using System.Net.Http;
-using System.Net;
 
 namespace CheckWord
 {
